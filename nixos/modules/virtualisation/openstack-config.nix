0b503b21e01a9afadbb237ecfe9d25dc4461e7bf
Graham Christensen <graham@grahamc.com>
2022-02-03 11:17:15 -0500

    openstack-config: support a ZFS root with /boot perhaps coming from an ESP

--- nixos/modules/virtualisation/openstack-config.nix
+++ nixos/modules/virtualisation/openstack-config.nix
@@ -1,6 +1,8 @@
-{ pkgs, lib, ... }:
+{ config, pkgs, lib, ... }:
 
 let
+  inherit (lib) mkIf mkDefault;
+  cfg = config.openstack;
   metadataFetcher = import ./openstack-metadata-fetcher.nix {
     targetRoot = "/";
     wgetExtraOptions = "--retry-connrefused";
@@ -16,26 +18,37 @@ in
   ];
 
   config = {
-    fileSystems."/" = {
+    fileSystems."/" = mkIf (!cfg.zfs.enable) {
       device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
       autoResize = true;
     };
 
+    fileSystems."/boot" = mkIf (cfg.efi || cfg.zfs.enable) {
+      # The ZFS image uses a partition labeled ESP whether or not we're
+      # booting with EFI.
+      device = "/dev/disk/by-label/ESP";
+      fsType = "vfat";
+    };
+
     boot.growPartition = true;
     boot.kernelParams = [ "console=ttyS0" ];
-    boot.loader.grub.device = "/dev/vda";
-    boot.loader.timeout = 0;
+    boot.loader.grub.device = if (!cfg.efi) then "/dev/vda" else "nodev";
+    boot.loader.grub.efiSupport = cfg.efi;
+    boot.loader.grub.efiInstallAsRemovable = cfg.efi;
+
+    services.zfs.expandOnBoot = mkIf cfg.zfs.enable "all";
+    boot.zfs.devNodes = mkIf cfg.zfs.enable "/dev/";
 
     # Allow root logins
     services.openssh = {
       enable = true;
       permitRootLogin = "prohibit-password";
-      passwordAuthentication = lib.mkDefault false;
+      passwordAuthentication = mkDefault false;
     };
 
     # Force getting the hostname from Openstack metadata.
-    networking.hostName = lib.mkDefault "";
+    networking.hostName = mkDefault "";
 
     systemd.services.openstack-init = {
       path = [ pkgs.wget ];
